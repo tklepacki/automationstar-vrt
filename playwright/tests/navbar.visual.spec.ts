f43f54430f23f57e12210c6bb514a0606f53a2dc
Tomasz Klepacki <t.klepacki@wp.pl>
2025-11-10 19:44:26 +0100

    Complete workshop source code

--- playwright/tests/navbar.visual.spec.ts
+++ playwright/tests/navbar.visual.spec.ts
@@ -1,16 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { debounceDom } from '../helpers/common';
-import tonnageList from '../fixtures/tonnageList.json';
-import tce from '../fixtures/tce.json';
 
-test('Signal Platform - Tonnage List', async ({ page }) => {
-    await page.route('/api/tonnageList?basisPortID**', async (route) => {
-        await route.fulfill({ json: tonnageList });
-    });
-    await page.route('/api/tonnageList/tce?tabId**', async (route) => {
-        await route.fulfill({ json: tce });
-    });
-
+test('Signal Platform - Navigation Bar', async ({ page }) => {
     await page.goto('/');
     await page.fill("#enterEmailFormEmail", "user1@stresstest.com");
     await page.click("#enterEmailFormSubmit");
@@ -19,13 +10,12 @@ test('Signal Platform - Tonnage List', async ({ page }) => {
     await page.getByRole("button", { name: "Accept" }).click();
     await page.waitForLoadState('networkidle')
     await page.waitForLoadState('domcontentloaded')
-    await page.goto('/tonnagelist');
-    await page.waitForLoadState('networkidle')
-    await page.waitForLoadState('domcontentloaded')
+    await page.waitForSelector('div[class*="SideNavCollapse_sideBar_"]')
     await debounceDom(page)
     await page.getByText('Favourites').click();
     await page.waitForLoadState('networkidle')
     await page.waitForLoadState('domcontentloaded')
     await debounceDom(page)
-    await expect(page).toHaveScreenshot(({ mask: [page.locator('.tw-flex-shrink-0'), page.locator('img.tw-h-4.tw-w-4')] }));
-});
+    await expect(page.locator('div[class*="SideNavCollapse_sideBar_"]')).toHaveScreenshot({ mask: [page.locator('.tw-flex-shrink-0'), page.locator('img.tw-h-4.tw-w-4')] });
+})
+
